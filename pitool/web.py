4cc543f33ec2dbba8fd8aa7fa17beba6e7dfd501
Colin Alston <colin.alston@gmail.com>
2016-03-24 15:16:41 +0200

    Neaten up utility classes

--- pitool/web.py
+++ pitool/web.py
@@ -1,56 +1,11 @@
-from twisted.web import server
-from twisted.web.resource import Resource
-from twisted.web.template import Element, renderer, XMLFile, tags, flattenString, TagLoader
+from twisted.web.template import renderer, XMLFile
 from twisted.python.filepath import FilePath
 
-
-class BaseElement(Element):
-    loader = XMLFile(FilePath('pitool/resources/base.html'))
-
-    def __init__(self, service, content):
-        Element.__init__(self)
-        self.content_element = content
-        self.service = service
-
-    @renderer
-    def header(self, request, tag):
-        return tag('1')
-
-    @renderer
-    def footer(self, request, tag):
-        return tag('2')
-
-    @renderer
-    def content(self, request, tag):
-        yield self.content_element(self.service)
-        #return tag("content")
-
-
-class ContentElement(Element):
-    def __init__(self, service):
-        Element.__init__(self)
-        self.service = service
-        
-
-class BaseResource(Resource):
-    def __init__(self, service):
-        self.service = service
-
-    def completeCall(self, response, request):
-        request.write(response)
-        request.finish()
-
-    def render_GET(self, request):
-        flattenString(None, BaseElement(self.service, self.Content)
-            ).addCallback(self.completeCall, request)
-
-        return server.NOT_DONE_YET
-
+from .web_base import BaseResource, ContentElement
 
 class Index(BaseResource):
     isLeaf = True
     addSlash = True
-    element = BaseElement
 
     class Content(ContentElement):
         loader = XMLFile(FilePath('pitool/resources/index.html'))
